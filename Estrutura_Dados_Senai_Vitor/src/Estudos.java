a797437a560d16d7e8404cb3b833ea4a381c4c8f
VitorSilva <vitor.s.conceicao6@aluno.senai.br>
2025-03-07 21:51:07 -0300

    aula1 - estrutura de dados

---
+++ Estrutura_Dados_Senai_Vitor/src/Estudos.java
@@ -0,0 +1,17 @@
+
+public class Estudos extends Aluno {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		aluno.nome = "Aluno Esforçado";
+		aluno.setIdade(34);
+		aluno.matricula = "09283421";
+		
+		System.out.println("Nome: " + aluno.nome + "\n" + 
+		"Idade: " + aluno.getIdade() + "\n" + 
+		"Matricula: " + aluno.matricula);
+		
+	}
+
+}
